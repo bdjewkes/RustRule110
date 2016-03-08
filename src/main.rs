0d8ddf07b7ba86b48939064062b64c9fe2d3c8e6
Brian Jewkes <brian@vicariousvr.com>
2016-03-07 16:32:33 -0800

    Cleaned up the 'rules' method. Generations are now expressed as an [bool; 256] as opposed to an array of integers of any flavor

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main(){
 	println!("<--------------------FIN-------------------->");
 }
 
-fn read_width(error: bool) -> usize{
+fn read_width(error: bool) -> u8{
 	if error {
 		println!("Invalid entry. Please specify your board with.");
 	} else {
@@ -21,13 +21,13 @@ fn read_width(error: bool) -> usize{
 	io::stdin().read_line(&mut width)
 			   .expect("Failed to read line");
 
-	match width.trim().parse::<usize>() {
+	match width.trim().parse::<u8>() {
  		Ok(n) => return n,	
  		Err(n) => return read_width(true),
  	}	
 }
 
-fn read_generations(error: bool) -> i32
+fn read_generations(error: bool) -> u32
 {
 	if error {
 		println!("Invalid entry. Please specify how many generations to run");
@@ -38,7 +38,7 @@ fn read_generations(error: bool) -> i32
 
 	io::stdin().read_line(&mut gen)
 			   .expect("Failed to read line");
-	match gen.trim().parse::<i32>() {
+	match gen.trim().parse::<u32>() {
  		Ok(n) => return n,	
  		Err(n) => read_generations(true),
   	}
@@ -46,53 +46,64 @@ fn read_generations(error: bool) -> i32
 
 
 
-fn generate_ca(gens: i32, width: usize){
-	let ruleset = [0,1,1,1,0,1,1,0];
-	let mut cells = [0; 150];
-	let mut next_cells = [0; 150];
-	
-	cells[width-1] = 1;
+fn generate_ca(gens: u32, board_width: u8){
+	let ruleset = [false,true,true,true,false,true,true,false];
+
+	let width = board_width as usize;
+	let mut cells = [false; 256];
+	let mut next_cells = [false; 256];
+
+	cells[width - 2] = true;
 
 	let mut current_gen = 0;
 
-	let alive = "░";
-	let dead = "█";
+	write_generation(cells, width);
 
-	//print the first generation
-	for x in 1..width-1{
-		if cells[x] == 1{
+	while current_gen < gens {
+		current_gen+=1;
+		for x in 1..width - 1 {
+		
+			let left_cell = cells[x - 1];
+			let current_cell = cells[x];
+			let right_cell = cells[x + 1];
+			
+			next_cells[x] = ruleset[get_rule_index(
+					left_cell, current_cell, right_cell) 
+					as usize];
+		}
+		cells = next_cells;	
+		write_generation(next_cells, width);
+		thread::sleep_ms(50);
+	}
+}
+
+fn get_rule_index(left: bool, current: bool, right: bool) -> u8 {
+	let prev_gen = (left, current, right);
+	match prev_gen
+	{
+		(true, true, true) => return 0,
+		(true, true, false) => return 1,
+		(true, false, true) => return 2,
+		(false, true, true) => return 3,
+		(true, false, false) => return 4,
+		(false, true, false) => return 5,
+		(false, false, true) => return 6,
+		(false, false, false) => return 7
+	}
+}
+
+fn write_generation(cells: [bool; 256], width: usize)
+{
+	let alive = "|";
+	let dead = " ";
+	//leave a buffer cell on either bound of the array
+	//the borders of the 'board' should always be 'dead's
+	for x in 1..width - 1{
+		if cells[x]{
 			print!("{}", alive)
 		}
 		else { print!("{}", dead)}
 	}
 	println!("");
-
-	while current_gen < gens {
-		current_gen+=1;
-		for x in 1..width-1{
-		
-			let left_cell = cells[x-1];
-			let current_cell = cells[x];
-			let right_cell = cells[x+1];
-				
-			let i = rules(left_cell, current_cell, right_cell);
-			next_cells[x] = ruleset[i as usize];
-				
-			if ruleset[i as usize] == 1{
-				print!("{}", alive);
-			}
-			else{ print!("{}", dead); }
-
-		}
-		cells = next_cells;		
-		println!("");
-		thread::sleep_ms(50);
-	}
-}
-
-fn rules(left: i32, current: i32, right: i32) -> u8 {
-	let s = left.to_string() + &current.to_string() + &right.to_string();
-	let x = u8::from_str_radix(&s, 2).unwrap();
-	return x;
 }
 
